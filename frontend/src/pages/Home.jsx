6b456b9a7bec2039fb15b96ed625b288885da50e
Percy Michael <percymichael0@gmail.com>
2025-02-18 18:36:46 +0300

    finished ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import axios from "axios";
+import Task from "../components/Task";
 
 const Home = () => {
   const { setUser, user } = useContext(AppContext);
@@ -66,193 +67,25 @@ const Home = () => {
       </header>
       <main className="bg-gradient-to-b from-slate-200 to-cyan-50 flex-1">
         <div className="min-w-full p-8 md:py-10 md:min-w-3/5 md:max-w-1/2 mx-auto">
-          <h3 className="font-bold text-lg p-3">Hello {user?.name}</h3>
+          <div className="p-3">
+            <h3 className="font-black text-2xl pb-2">Hello, {user?.name}!</h3>
+            <p className="text-sm text-gray-600">
+              Welcome back to your task management dashboard. Let's get started!
+            </p>
+          </div>
           <ul className="list bg-base-100 rounded-box shadow-md">
-            <li className="p-4 pb-2 text-xs tracking-wide flex justify-between items-center">
-              <span>Most played songs this week</span>
-              <button
-                className="btn btn-success"
-                onClick={() =>
-                  document.getElementById("my_modal_1").showModal()
-                }
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2.5"
-                  stroke="currentColor"
-                  className="size-[1.2em]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                  />
-                </svg>
-                New Task
-              </button>
+            <div className="p-4 pb-2 text-xs w-full flex justify-between items-center">
+              <h3 className="text-lg font-bold px-5 opacity-50">Tasks</h3>
+              <button className="btn btn-success">Success</button>
+            </div>
 
-              <dialog id="my_modal_1" className="modal">
-                <div className="modal-box">
-                  <h3 className="font-bold text-lg">Hello!</h3>
-                  <p className="py-4">
-                    Press ESC key or click the button below to close
-                  </p>
-                  <div className="modal-action">
-                    <form method="dialog">
-                      {/* if there is a button in form, it will close the modal */}
-                      <button className="btn">Close</button>
-                    </form>
-                  </div>
-                </div>
-              </dialog>
-            </li>
-
-            <li className="list-row">
-              <div>
-                <img
-                  className="size-10 rounded-box"
-                  src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-                />
-              </div>
-              <div>
-                <div>Dio Lupa</div>
-                <div className="text-xs font-semibold opacity-60">
-                  Assigned to
-                </div>
-              </div>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                  />
-                </svg>
-              </button>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  />
-                </svg>
-              </button>
-            </li>
-
-            <li className="list-row">
-              <div>
-                <img
-                  className="size-10 rounded-box"
-                  src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-                />
-              </div>
-              <div>
-                <div>Ellie Beilish</div>
-                <div className="text-xs uppercase font-semibold opacity-60">
-                  Bears of a fever
-                </div>
-              </div>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                  >
-                    <path d="M6 3L20 12 6 21 6 3z"></path>
-                  </g>
-                </svg>
-              </button>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                  >
-                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                  </g>
-                </svg>
-              </button>
-            </li>
-
-            <li className="list-row">
-              <div>
-                <img
-                  className="size-10 rounded-box"
-                  src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-                />
-              </div>
-              <div>
-                <div>Sabrino Gardener</div>
-                <div className="text-xs uppercase font-semibold opacity-60">
-                  Cappuccino
-                </div>
-              </div>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                  >
-                    <path d="M6 3L20 12 6 21 6 3z"></path>
-                  </g>
-                </svg>
-              </button>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                  >
-                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                  </g>
-                </svg>
-              </button>
-            </li>
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
           </ul>
         </div>
       </main>
